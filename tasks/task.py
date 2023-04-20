a05ed6dad120b244387fccce15eeac227edf7729
Gruzin <gruzd.roman@inbox.ru>
2023-04-20 16:36:43 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -4,4 +4,17 @@ from typing import List
 class Counter:
     def __init__(self, values: List[int]):
         self.values = values
-    # TODO: add your code here
+
+    def __add__(self, b):
+        num = []
+        for i in self.values:
+            num.append(str(i) + ' ' + b)
+        return num
+
+
+
+
+
+counter = Counter([1, 2, 3]) + "mississippi"
+print(counter)
+# ["1 mississippi", "2 mississippi" , "3 mississippi"]
